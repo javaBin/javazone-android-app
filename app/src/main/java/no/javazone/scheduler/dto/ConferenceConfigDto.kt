62130938d4f779e48a166e8bf63297a4ca667f03
John L Fjellstad <john@fjellstad.org>
2020-05-30 13:50:24 +0200

    convert from json to domain models

---
+++ app/src/main/java/no/javazone/scheduler/dto/ConferenceConfigDto.kt
@@ -0,0 +1,11 @@
+package no.javazone.scheduler.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ConferenceConfigDto(
+    val conferenceName: String,
+    val workshopDate: String,
+    val conferenceDates: List<String>,
+    val conferenceUrl: String
+)
---
+++ app/src/test/java/no/javazone/scheduler/dto/ConferenceConfigDtoTest.kt
@@ -0,0 +1,28 @@
+package no.javazone.scheduler.dto
+
+import com.google.common.truth.Truth.assertThat
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import org.junit.jupiter.api.Test
+import java.nio.file.Files
+import java.nio.file.Paths
+
+internal class ConferenceConfigDtoTest {
+    private val path = Paths.get("src/test/res")
+
+    @Test
+    fun test() {
+        val expected = ConferenceConfigDto(
+            conferenceName = "JavaZone 2019",
+            workshopDate = "10.09.2019",
+            conferenceDates = listOf("11.09.2019", "12.09.2019"),
+            conferenceUrl = "https://sleepingpill.javazone.no/public/allSessions/javazone_2019")
+        val jsonStringBuffer = String(Files.readAllBytes(path.resolve("config.json")))
+
+        val json = Json(JsonConfiguration.Stable)
+        val result = json.parse(ConferenceConfigDto.serializer(), jsonStringBuffer)
+
+        assertThat(result).isNotNull()
+        assertThat(result).isEqualTo(expected)
+    }
+}
