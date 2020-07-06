4180b27d4f9e6bb75c7aacee457407e790b39ad3
John L Fjellstad <john@fjellstad.org>
2020-07-06 22:39:36 +0200

    added modelview

--- app/src/main/java/no/javazone/scheduler/repository/ConferenceSessionRepository.kt
+++ app/src/main/java/no/javazone/scheduler/repository/ConferenceSessionRepository.kt
@@ -2,8 +2,11 @@ package no.javazone.scheduler.repository
 
 import androidx.lifecycle.LiveData
 import no.javazone.scheduler.model.ConferenceSession
+import no.javazone.scheduler.model.Talk
 import java.time.LocalDate
 
 interface ConferenceSessionRepository {
     fun getSessions(date: LocalDate): LiveData<List<ConferenceSession>>
+    fun getSession(sessionId: Long): LiveData<ConferenceSession>
+    fun getTalk(talkId: String): LiveData<Talk>
 }
