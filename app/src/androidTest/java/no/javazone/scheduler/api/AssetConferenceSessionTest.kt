a5f307ddef843a323d69dd5311e1322c3f30d554
John L Fjellstad <john@fjellstad.org>
2021-11-06 14:20:52 +0100

    fix build error for testing

--- app/src/androidTest/java/no/javazone/scheduler/api/AssetConferenceSessionTest.kt
+++ app/src/androidTest/java/no/javazone/scheduler/api/AssetConferenceSessionTest.kt
@@ -1,13 +1,10 @@
 package no.javazone.scheduler.api
 
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class AssetConferenceSessionTest {
     private lateinit var api: ConferenceSessionApi
 
