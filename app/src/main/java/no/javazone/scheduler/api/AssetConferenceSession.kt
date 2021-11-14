7b4833d42d622d37d30904af7bd1476c8aff2e45
John L Fjellstad <john@fjellstad.org>
2021-11-14 15:16:50 +0100

    send dispatchers in as a dependency

--- app/src/main/java/no/javazone/scheduler/api/AssetConferenceSession.kt
+++ app/src/main/java/no/javazone/scheduler/api/AssetConferenceSession.kt
@@ -1,6 +1,7 @@
 package no.javazone.scheduler.api
 
 import android.content.Context
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
@@ -11,7 +12,8 @@ import java.io.InputStream
 import java.io.InputStreamReader
 
 class AssetConferenceSession(
-    private val inputStream: InputStream
+    private val inputStream: InputStream,
+    private val dispatchers: CoroutineDispatcher
 ) : ConferenceSessionApi {
     private val sessions: List<ConferenceSession> by lazy {
         val jsonStringBuffer = InputStreamReader(inputStream).readText()
@@ -20,7 +22,7 @@ class AssetConferenceSession(
     }
 
     override suspend fun fetch(): List<ConferenceSession> =
-        withContext(Dispatchers.IO) {
+        withContext(dispatchers) {
             sessions
         }
 
@@ -30,14 +32,18 @@ class AssetConferenceSession(
         @Volatile
         private var instance: ConferenceSessionApi? = null
 
-        fun getInstance(context: Context): ConferenceSessionApi =
-            instance ?: synchronized(this) {
-                instance ?: AssetConferenceSession(context.assets.open(ASSET_NAME))
-            }
+        fun getInstance(
+            context: Context,
+            dispatchers: CoroutineDispatcher = Dispatchers.IO
+        ): ConferenceSessionApi =
+            getInstance(context.assets.open(ASSET_NAME), dispatchers)
 
-        fun getInstance(inputStream: InputStream): ConferenceSessionApi =
+        fun getInstance(
+            inputStream: InputStream,
+            dispatchers: CoroutineDispatcher = Dispatchers.IO
+        ): ConferenceSessionApi =
             instance ?: synchronized(this) {
-                instance ?: AssetConferenceSession(inputStream)
+                instance ?: AssetConferenceSession(inputStream, dispatchers)
             }
 
     }
