1798f2dc13ce71721c06f7328164c166488c0d23
Sverre Moe <sverre.moe@gmail.com>
2026-05-13 10:51:35 +0200

    Extract Session Section Header Component

---
+++ app/src/main/java/no/javazone/scheduler/ui/components/SessionSectionHeader.kt
@@ -0,0 +1,50 @@
+package no.javazone.scheduler.ui.components
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import no.javazone.scheduler.ui.theme.JavaZoneTheme
+
+@Composable
+fun SessionSectionHeader(text: String) {
+    Surface(
+        tonalElevation = 10.dp,
+        modifier = Modifier.padding(vertical = 8.dp)
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Column(
+                modifier = Modifier
+                    .padding(start = 10.dp, end = 10.dp)
+                    .weight(1f)
+            ) {
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun SessionSectionHeaderLightPreview() {
+    SessionSectionHeader(text = "09:00")
+}
+
+@Composable
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+fun SessionSectionHeaderDarkPreview() {
+    JavaZoneTheme(useDarkTheme = true) {
+        SessionSectionHeader(text = "09:00")
+    }
+}
--- app/src/main/java/no/javazone/scheduler/ui/schedules/MyScheduleRoute.kt
+++ app/src/main/java/no/javazone/scheduler/ui/schedules/MyScheduleRoute.kt
@@ -3,19 +3,12 @@ package no.javazone.scheduler.ui.schedules
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
@@ -23,11 +16,14 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import no.javazone.scheduler.model.ConferenceTalk
 import no.javazone.scheduler.ui.components.DetailsScreen
+import no.javazone.scheduler.ui.components.SessionSectionHeader
 import no.javazone.scheduler.ui.components.TalkCard
 import no.javazone.scheduler.ui.theme.JavaZoneTheme
 import no.javazone.scheduler.ui.theme.SessionDayFormat
+import no.javazone.scheduler.ui.theme.SessionTimeFormat
 import no.javazone.scheduler.utils.LOG_TAG
 import no.javazone.scheduler.utils.sampleTalks
+import no.javazone.scheduler.utils.toLocalString
 import no.javazone.scheduler.viewmodels.ConferenceListViewModel
 import java.time.LocalDate
 
@@ -69,18 +65,7 @@ private fun MyScheduleScreen(
         ) {
             conferenceTalks.forEach { (slot, talks) ->
                 stickyHeader {
-                    Surface(tonalElevation = 10.dp) {
-                        Row(modifier = Modifier.fillMaxWidth()) {
-                            Column(
-                                modifier = Modifier.padding(start = 10.dp, end = 10.dp)
-                            ) {
-                                Text(
-                                    text = slot.format(SessionDayFormat),
-                                    style = MaterialTheme.typography.headlineLarge
-                                )
-                            }
-                        }
-                    }
+                    SessionSectionHeader(text = slot.format(SessionDayFormat))
                 }
 
                 items(talks) { talk ->
